2b4f0a65253869fbf3446a9900d5be6fa8259899
Peter Lind (@KEA) <petl@kea.dk>
2023-06-22 09:35:28 +0200

    first try, get and post

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import fs from 'fs/promises'
+const app = express();
+
+app.use(express.json());
+
+app.listen(4000, () =>
+    console.log("Example app listening on port 4000!"),
+)
+
+app.get("/", (req,res) => {
+    res.send("Hello from Node halløhj!");
+})
+
+app.get("/users", (req,res) => {
+    res.json(["Ramsus", "Petre"]);
+})
+
+app.get("/students", async (req,res) => {
+    const students = JSON.parse(await fs.readFile("data.json"));
+//    console.log(students);
+    res.json(students);
+})
+
+app.get("/student/:id", async (req,res) => {
+    const students = JSON.parse(await fs.readFile("data.json"));
+    const id = req.params.id;
+
+    const student = students.find(student => student.fullname.toLowerCase().includes(id.toLowerCase()));
+    console.log("looking for: " + id);
+    res.json(student);
+});
+
+app.post("/students", async (req,res) => {
+    const students = JSON.parse(await fs.readFile("data.json"));
+    const newStudent = req.body;
+    console.log("new student: ",newStudent);
+
+    const id = new Date().getTime();
+    newStudent.id = id;
+    students.push(newStudent);
+
+    fs.writeFile("data.json", JSON.stringify(students));
+
+    res.json(students);
+})
