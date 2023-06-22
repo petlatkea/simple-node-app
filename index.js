8d08ddef6c4a72f3dcc58e7894e04e94c4b1fbb0
Peter Lind (@KEA) <petl@kea.dk>
2023-06-22 09:52:40 +0200

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import fs from 'fs/promises'
+import cors from 'cors';
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(4000, () =>
     console.log("Example app listening on port 4000!"),
